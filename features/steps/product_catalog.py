1f7b9c52cdf1dcac68e4c1f1924ce774c09006d8
au49218 <bucksbin2k1@yahoo.com>
2021-02-14 00:44:19 -0600

    Lesson 4 Homework 0

---
+++ features/steps/product_catalog.py
@@ -0,0 +1,10 @@
+from behave import when
+from selenium.webdriver.common.by import By
+
+PRODUCT_RESULT = (By.XPATH, "//div[@data-component-type='s-search-result']//a[.//span[@class='a-price']]")
+
+
+@when('Click on the first product')
+def click_first_product(context):
+    # find the element for product result
+    context.driver.find_element(*PRODUCT_RESULT).click()
