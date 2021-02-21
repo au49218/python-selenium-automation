1b91bdacb8c56ccb63158b90324c0cf231516ef9
au49218 <bucksbin2k1@yahoo.com>
2021-02-20 22:21:13 -0600

    Lesson 5 Homework

---
+++ features/steps/sign_in_popup.py
@@ -0,0 +1,24 @@
+from behave import when, then, given
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+SIGN_IN_POPUP_BTN = (By.CSS_SELECTOR, "#nav-signin-tooltip .nav-action-inner")
+
+
+@when('Click Sign In from popup')
+def click_sign_in_popup_btn(context):
+    # wait for x seconds until clickable
+    e = context.driver.wait.until(EC.element_to_be_clickable(SIGN_IN_POPUP_BTN))
+    # click the button
+    e.click()
+
+
+@then('Verify Sign In page opens')
+def verify_sign_in_page_opens(context):
+    # wait for x seconds until url contains value
+    context.driver.wait.until(EC.url_contains('https://www.amazon.com/ap/signin'),
+    f'URL {context.driver.current_url} does not include https://www.amazon.com/ap/signin')
+
+    # verify actual match the expected
+    assert 'https://www.amazon.com/ap/signin' in context.driver.current_url, \
+    f'URL {context.driver.current_url} does not include https://www.amazon.com/ap/signin'
