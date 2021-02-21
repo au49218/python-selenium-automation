94e7dde17942d1d1bdd168da61d2b668747eae7b
au49218 <bucksbin2k1@yahoo.com>
2021-02-20 22:20:39 -0600

    Lesson 5 Homework

--- features/steps/product_page.py
+++ features/steps/product_page.py
@@ -8,6 +8,8 @@ SIZE_TOOLTIP = (By.ID, 'a-popover-content-1')
 SIZE_SELECTION_BTN = (By.ID, 'dropdown_selected_size_name')
 SIZE_OPTION_0 = (By.ID, 'size_name_0')
 
+COLOR_OPTIONS = (By.CSS_SELECTOR, "#variation_color_name li")
+SELECTED_COLOR = (By.CSS_SELECTOR, "#variation_color_name .selection")
 
 @when('Click on Add to cart button')
 def click_add_to_cart(context):
@@ -34,3 +36,16 @@ def verify_cart_count(context, expected_count):
     cart_count = context.driver.find_element(*CART).text
     # verify expected count match cart count
     assert expected_count == cart_count, f'Expected {expected_count} items'
+
+
+@then('Verify user can click through colors')
+def verify_can_select_colors(context):
+    # find the elements for colors
+    expected_colors = ['Black', 'Blue', 'Burgundy', 'White Lace', 'Off White', 'Pink', 'Navy']
+    colors = context.driver.find_elements(*COLOR_OPTIONS)
+
+    # Web Element 1, Web Element 2, Web Element 3
+    for i in range(len(colors)):
+        colors[i].click()
+        selected_color = context.driver.find_element(*SELECTED_COLOR).text
+        assert expected_colors[i] == selected_color, f'Expected {expected_colors[i]} but got {selected_color}'
