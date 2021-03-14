112e0e9aa2d7978eacad0ff2172bc1a16cc8abb0
au49218 <bucksbin2k1@yahoo.com>
2021-03-14 03:46:13 -0600

    Lesson 7 Homework

--- features/environment.py
+++ features/environment.py
@@ -1,3 +1,5 @@
+from app.application import Application
+
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -13,7 +15,8 @@ def browser_init(context):
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
 
-    context.driver.wait = WebDriverWait(context.driver, 1)
+    context.driver.wait = WebDriverWait(context.driver, 10)
+    context.app = Application(context.driver)
 
 
 def before_scenario(context, scenario):
