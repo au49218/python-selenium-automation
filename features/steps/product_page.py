f1a250f4fdd3be07a27f4c3432ab92cf70d0a135
au49218 <bucksbin2k1@yahoo.com>
2021-03-14 03:46:33 -0600

    Lesson 7 Homework

--- features/steps/product_page.py
+++ features/steps/product_page.py
@@ -11,18 +11,22 @@ SIZE_OPTION_0 = (By.ID, 'size_name_0')
 COLOR_OPTIONS = (By.CSS_SELECTOR, "#variation_color_name li")
 SELECTED_COLOR = (By.CSS_SELECTOR, "#variation_color_name .selection")
 
+
 @when('Click on Add to cart button')
 def click_add_to_cart(context):
     # find the element for add to cart button
-    context.driver.find_element(*ADD_TO_CART_BTN).click()
+    # context.driver.find_element(*ADD_TO_CART_BTN).click()
+
+    # find the element for add to cart button using Page Object
+    context.app.product_page.click_add_to_cart()
 
 
 @when('Select shoes size')
 def select_size(context):
     # find the element for size selection button
-    context.driver.find_element(*SIZE_SELECTION_BTN).click()
+    # context.driver.find_element(*SIZE_SELECTION_BTN).click()
     # find the element for size option
-    context.driver.find_element(*SIZE_OPTION_0).click()
+    # context.driver.find_element(*SIZE_OPTION_0).click()
     # verify the size tooltip equal 1
     # if len(context.driver.find_elements(*SIZE_TOOLTIP)) == 1:
     #    context.driver.find_element(*SIZE_SELECTION_BTN).click()
@@ -30,22 +34,31 @@ def select_size(context):
     #    sleep(2)
     #    context.driver.find_element(*ADD_TO_CART_BTN).click()
 
+    # find the element for size selection and size option using Page Object
+    context.app.product_page.select_size()
+
 
 @then('Verify cart has {expected_count} item')
 def verify_cart_count(context, expected_count):
-    cart_count = context.driver.find_element(*CART).text
+    # cart_count = context.driver.find_element(*CART).text
     # verify expected count match cart count
-    assert expected_count == cart_count, f'Expected {expected_count} items'
+    # assert expected_count == cart_count, f'Expected {expected_count} items'
+
+    # verify expected count match cart count using Page Object
+    context.app.product_page.verify_cart_count(expected_count)
 
 
 @then('Verify user can click through colors')
 def verify_can_select_colors(context):
     # find the elements for colors
-    expected_colors = ['Black', 'Blue', 'Burgundy', 'White Lace', 'Off White', 'Pink', 'Navy']
-    colors = context.driver.find_elements(*COLOR_OPTIONS)
+    # expected_colors = ['Black', 'Blue', 'Burgundy', 'White Lace', 'Off White', 'Pink', 'Navy']
+    # colors = context.driver.find_elements(*COLOR_OPTIONS)
 
     # Web Element 1, Web Element 2, Web Element 3
-    for i in range(len(colors)):
-        colors[i].click()
-        selected_color = context.driver.find_element(*SELECTED_COLOR).text
-        assert expected_colors[i] == selected_color, f'Expected {expected_colors[i]} but got {selected_color}'
+    # for i in range(len(colors)):
+        # colors[i].click()
+        # selected_color = context.driver.find_element(*SELECTED_COLOR).text
+        # assert expected_colors[i] == selected_color, f'Expected {expected_colors[i]} but got {selected_color}'
+
+    # Verify selection of colors using Page Object
+    context.app.product_page.verify_can_select_dress_colors()
