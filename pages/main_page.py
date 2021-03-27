d868cc5ab55cd7e2c89fa8cff575a2e2afa7d961
au49218 <bucksbin2k1@yahoo.com>
2021-03-27 03:52:42 -0500

    Lesson 9 Homework

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,10 +1,12 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 from pages.base_page import Page
 
 
 class MainPage(Page):
     SEARCH_FIELD = (By.ID, 'twotabsearchtextbox')
     SEARCH_ICON = (By.ID, 'nav-search-submit-button')
+    SEARCH_DROPDOWN = (By.CSS_SELECTOR, 'select.nav-search-dropdown')
 
     def open_main_page(self):
         self.open_page('https://www.amazon.com/')
@@ -19,3 +21,7 @@ class MainPage(Page):
     def search_for_product(self, search_query):
         self.input_text(search_query, *self.SEARCH_FIELD)
         self.find_element(*self.SEARCH_FIELD).send_keys(Keys.Enter)
+
+    def select_department(self, alias: str):
+        select = Select(self.find_element(*self.SEARCH_DROPDOWN))
+        select.select_by_value(f'search-alias={alias}')
